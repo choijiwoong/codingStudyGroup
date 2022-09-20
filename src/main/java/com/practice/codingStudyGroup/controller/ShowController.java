5fcc9fc416130a88de7c9616edb96f42ef7fae50
Choijiwoong <gogogi313@naver.com>
2022-09-20 20:55:32 +0900

    Initial commit

---
+++ src/main/java/com/practice/codingStudyGroup/controller/ShowController.java
@@ -0,0 +1,13 @@
+package com.practice.codingStudyGroup.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ShowController {
+
+    @GetMapping("/show")
+    public String show(){
+        return "show.html";
+    }
+}
